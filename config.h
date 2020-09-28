ebe0dd7a16c65ca13409ba4ac471e1409fed7c9d
Lemoners <987485970@qq.com>
2020-09-28 19:25:49 +0800

    Updating scratch pad

--- config.h
+++ config.h
@@ -3,6 +3,12 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap betwe    en windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap betwee    n windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap betwe    en windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap betwee    n windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap     when there is only one window */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -60,7 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "gnome-terminal", "-t", scratchpadname, "-g", "480x128", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x32", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
