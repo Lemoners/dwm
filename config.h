0f2313818bf2169121917649d6da0cd32cc97305
Lemoners <987485970@qq.com>
2020-10-31 21:42:26 +0800

    Updating scratch pad

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x32", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x32", NULL };
 static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
 static const char *googlecmd[] = {"google-chrome", NULL};
 
